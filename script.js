ddf398dc7d57c82510e431b00456e65cee68a70e
tmmgn <tmmgn1337@gmail.com>
2024-09-12 16:21:56 +0300

    script fixes according to last user settings changes

--- script.js
+++ script.js
@@ -112,135 +112,166 @@ const normalizeWlConfig = (cfg) => {
 
 const round = (num) => Math.round(num * 100) / 100;
 
+function isParent(node) {
+  return node.direction != null;
+}
+
+function getLeaves(tree) {
+  if (tree == null) {
+    return [];
+  } else if (isParent(tree)) {
+    return getLeaves(tree.first).concat(getLeaves(tree.second));
+  } else {
+    return [tree];
+  }
+}
+
 function analyzeFile(data) {
   try {
     const parsedFile = JSON.parse(data);
     const result = parsedFile.reduce(
       (acc, setting) => {
         try {
-          const userId = setting.user_id;
+          //   const userId = setting.user_id;
           const parsedState = JSON.parse(setting.state);
-          const { trade = {}, style = {}, tileBoard, watchlists } = parsedState;
-          const isTradingLocked = trade.isTradingLocked;
-          const closeConfirmation = trade.closeConfirmation;
-          const colorizedQuotes = style.colorizedQuotes;
-          const quickTradeEnabled = trade.quick?.enabled;
-          const boardsCount = tileBoard?.boards?.length || 0;
-          const wlCount = watchlists?.length || 0;
-          let emptyWlCount = 0;
-          let wlCountFromBoard = 0;
-          let wlIds = [];
-          const panels = tileBoard?.boards || [];
-          let widgetsConfig = [];
-          let singleBoardWidgetsConfig = [];
-          let multiBoardWidgetsConfig = [];
-          let multiBoardWidgetsConfigByUser = {};
-          const isMultiBoard = panels?.length > 1;
-          let widgetsCountForEachBoard = [];
-          if (panels?.length) {
-            const configs = panels.map((p) => {
-              if (p.watchlists.length) {
-                wlCountFromBoard = wlCountFromBoard + p.watchlists.length; // Count watchlists that present in board
-                wlIds = [...wlIds, ...p.watchlists];
+          const accountsSettings = parsedState.account
+            ? Object.entries(parsedState.account)
+            : [];
+          //   const membersSettings = parsedState.account
+          //     ? Object.values(parsedState.member)
+          //     : [];
+
+          accountsSettings.forEach(([accountId, settings]) => {
+            const { trade = {}, v2, watchlists, boards } = settings;
+            const isTradingLocked = trade.isTradingLocked;
+            const closeConfirmation = trade.closeConfirmation;
+            const quickTradeEnabled = trade.quick?.enabled;
+            const boardsCount = v2?.tileBoard?.boards?.length || 0;
+            const wlCount = watchlists?.length || 0;
+            let emptyWlCount = 0;
+            let wlCountFromBoard = 0;
+            let wlIds = [];
+            const panels = v2?.tileBoard?.boards || [];
+            let widgetsConfig = [];
+            let singleBoardWidgetsConfig = [];
+            let multiBoardWidgetsConfig = [];
+            const isMultiBoard = panels?.length > 1;
+            let widgetsCountForEachBoard = [];
+            const structure = settings?.structure;
+            const widgets = settings?.widgets;
+            if (panels?.length) {
+              const configs = panels.map((boardId) => {
+                const mosaicStructure = structure?.[boardId];
+                const widgetIds = getLeaves(mosaicStructure)?.filter(
+                  (leave) => Object.keys(leave).length
+                );
+                const boardWatchlists = boards[boardId]?.watchlists;
+                if (boardWatchlists?.length) {
+                  wlCountFromBoard = wlCountFromBoard + boardWatchlists.length; // Count watchlists that present in board
+                  wlIds = [...wlIds, ...boardWatchlists];
+                }
+
+                let widgetsCount = {};
+                const widgetsOnBoard = widgetIds
+                  .map((w) => {
+                    const name = widgets[w].component;
+                    widgetsCount[name] = (widgetsCount[name] || 0) + 1;
+                    // if (name === CHART_NAME) {
+                    //   const view = p.widgets[w]?.config.view || 'simple';
+                    // }
+                    return name;
+                  })
+                  .sort();
+                widgetsCountForEachBoard = Object.keys(widgetsCount).length
+                  ? [...widgetsCountForEachBoard, widgetsCount]
+                  : widgetsCountForEachBoard;
+
+                return _.uniq(widgetsOnBoard).join(","); // get unique widget names from board config
+              });
+              widgetsConfig = configs;
+              if (isMultiBoard) {
+                multiBoardWidgetsConfig = configs; // If board is not single add config to multiple
+              } else {
+                singleBoardWidgetsConfig = configs; // If board is single add config to single
               }
-              let widgetsCount = {};
-              const widgetsOnBoard = Object.keys(p.widgets)
-                .map((w) => {
-                  const name = p.widgets[w].component;
-                  widgetsCount[name] = (widgetsCount[name] || 0) + 1;
-                  // if (name === CHART_NAME) {
-                  //   const view = p.widgets[w]?.config.view || 'simple';
-                  // }
-                  return name;
-                })
-                .sort();
-              widgetsCountForEachBoard = Object.keys(widgetsCount).length
-                ? [...widgetsCountForEachBoard, widgetsCount]
-                : widgetsCountForEachBoard;
-              return _.uniq(widgetsOnBoard).join(","); // get unique widget names from board config
-            });
-            widgetsConfig = configs;
-            if (isMultiBoard) {
-              multiBoardWidgetsConfig = configs; // If board is not single add config to multiple
-            } else {
-              singleBoardWidgetsConfig = configs; // If board is single add config to single
             }
-          }
-          const normalizedWidgetsCountForEachBoard =
-            widgetsCountForEachBoard.filter(Boolean);
-          const instrumentsCount = watchlists?.length
-            ? watchlists
-                .filter((wl) => wlIds.includes(wl.id)) // Filter by wl ids that present in boards
-                .reduce((acc, wl) => {
-                  const isEmpty = !wl.instruments?.length;
-                  if (isEmpty) {
-                    emptyWlCount = emptyWlCount + 1; // Count empty watchlists
-                  }
-                  return acc + wl.instruments?.length || 0;
-                }, 0)
-            : 0;
-          return {
-            usersCount: acc.usersCount + 1,
-            userWithNoBoard: !boardsCount
-              ? acc.userWithNoBoard + 1
-              : acc.userWithNoBoard,
-            usersWithBoard: boardsCount
-              ? acc.usersWithBoard + 1
-              : acc.usersWithBoard,
-            usersWithMultiBoard: isMultiBoard
-              ? acc.usersWithMultiBoard + 1
-              : acc.usersWithMultiBoard,
-            usersWithWatchlists: wlCount
-              ? acc.usersWithWatchlists + 1
-              : acc.usersWithWatchlists,
-            usersWithWatchlistOnBoard: wlCountFromBoard
-              ? acc.usersWithWatchlistOnBoard + 1
-              : acc.usersWithWatchlistOnBoard,
-            emptyWlCount: emptyWlCount
+            const normalizedWidgetsCountForEachBoard =
+              widgetsCountForEachBoard.filter(Boolean);
+            const instrumentsCount = watchlists?.length
+              ? watchlists
+                  .filter((wl) => wlIds.includes(wl.id)) // Filter by wl ids that present in boards
+                  .reduce((acc, wl) => {
+                    const isEmpty = !wl.instruments?.length;
+                    if (isEmpty) {
+                      emptyWlCount = emptyWlCount + 1; // Count empty watchlists
+                    }
+                    return acc + wl.instruments?.length || 0;
+                  }, 0)
+              : 0;
+            (acc.accountsCount = acc.accountsCount + 1),
+              (acc.accountWithNoBoard = !boardsCount
+                ? acc.accountWithNoBoard + 1
+                : acc.accountWithNoBoard);
+            acc.accountsWithBoard = boardsCount
+              ? acc.accountsWithBoard + 1
+              : acc.accountsWithBoard;
+            acc.accountsWithMultiBoard = isMultiBoard
+              ? acc.accountsWithMultiBoard + 1
+              : acc.accountsWithMultiBoard;
+            acc.accountsWithWatchlists = wlCount
+              ? acc.accountsWithWatchlists + 1
+              : acc.accountsWithWatchlists;
+            acc.accountsWithWatchlistOnBoard = wlCountFromBoard
+              ? acc.accountsWithWatchlistOnBoard + 1
+              : acc.accountsWithWatchlistOnBoard;
+            acc.emptyWlCount = emptyWlCount
               ? acc.emptyWlCount + 1
-              : acc.emptyWlCount,
-            isTradingLocked: isTradingLocked
+              : acc.emptyWlCount;
+            acc.isTradingLocked = isTradingLocked
               ? acc.isTradingLocked + 1
-              : acc.isTradingLocked,
-            closeConfirmation: closeConfirmation
+              : acc.isTradingLocked;
+            acc.closeConfirmation = closeConfirmation
               ? acc.closeConfirmation + 1
-              : acc.closeConfirmation,
-            colorizedQuotes: colorizedQuotes
-              ? acc.colorizedQuotes + 1
-              : acc.colorizedQuotes,
-            quickTradeEnabled: quickTradeEnabled
+              : acc.closeConfirmation;
+            acc.quickTradeEnabled = quickTradeEnabled
               ? acc.quickTradeEnabled + 1
-              : acc.quickTradeEnabled,
-            boardsCount: acc.boardsCount + boardsCount,
-            wlCount: acc.wlCount + wlCount,
-            wlCountFromBoard: acc.wlCountFromBoard + wlCountFromBoard,
-            wlCountFromBoardByUser: wlCountFromBoard // Adds value only if we have watchlists on boards for median value
-              ? [...acc.wlCountFromBoardByUser, wlCountFromBoard]
-              : acc.wlCountFromBoardByUser,
-            instrumentsCountByUser: instrumentsCount // Adds value only if we have instruments for median value
-              ? [...acc.instrumentsCountByUser, instrumentsCount]
-              : acc.instrumentsCountByUser,
-            instrumentsCount: acc.instrumentsCount + (instrumentsCount || 0),
-            widgetsConfig: _.sortBy([...acc.widgetsConfig, ...widgetsConfig]),
-            singleBoardWidgetsConfig: _.sortBy([
+              : acc.quickTradeEnabled;
+            acc.boardsCount = acc.boardsCount + boardsCount;
+            acc.wlCount = acc.wlCount + wlCount;
+            acc.wlCountFromBoard = acc.wlCountFromBoard + wlCountFromBoard;
+            acc.wlCountFromBoardByAccount = wlCountFromBoard // Adds value only if we have watchlists on boards for median value
+              ? [...acc.wlCountFromBoardByAccount, wlCountFromBoard]
+              : acc.wlCountFromBoardByAccount;
+            acc.instrumentsCountByAccount = instrumentsCount // Adds value only if we have instruments for median value
+              ? [...acc.instrumentsCountByAccount, instrumentsCount]
+              : acc.instrumentsCountByAccount;
+            acc.instrumentsCount =
+              acc.instrumentsCount + (instrumentsCount || 0);
+            acc.widgetsConfig = _.sortBy([
+              ...acc.widgetsConfig,
+              ...widgetsConfig,
+            ]);
+            acc.singleBoardWidgetsConfig = _.sortBy([
               ...acc.singleBoardWidgetsConfig,
               ...singleBoardWidgetsConfig,
-            ]),
-            multiBoardWidgetsConfig: _.sortBy([
+            ]);
+            acc.multiBoardWidgetsConfig = _.sortBy([
               ...acc.multiBoardWidgetsConfig,
               ...multiBoardWidgetsConfig,
-            ]),
-            widgetsCountsByBoards: [
+            ]);
+            acc.widgetsCountsByBoards = [
               ...acc.widgetsCountsByBoards,
               normalizedWidgetsCountForEachBoard,
-            ].filter((c) => c.length),
-            multiBoardWidgetsConfigByUser: isMultiBoard
+            ].filter((c) => c.length);
+            acc.multiBoardWidgetsConfigByAccount = isMultiBoard
               ? {
-                  ...acc.multiBoardWidgetsConfigByUser,
-                  [userId]: multiBoardWidgetsConfig,
+                  ...acc.multiBoardWidgetsConfigByAccount,
+                  [accountId]: multiBoardWidgetsConfig,
                 }
-              : acc.multiBoardWidgetsConfigByUser,
-          };
+              : acc.multiBoardWidgetsConfigByAccount;
+          });
+          acc.usersCount = acc.usersCount + 1;
+          return acc;
         } catch (err) {
           console.warn(err);
           return acc;
@@ -248,15 +279,15 @@ function analyzeFile(data) {
       },
       {
         usersCount: 0,
-        userWithNoBoard: 0,
-        usersWithBoard: 0,
-        usersWithMultiBoard: 0,
-        usersWithWatchlists: 0,
-        usersWithWatchlistOnBoard: 0,
+        accountsCount: 0,
+        accountWithNoBoard: 0,
+        accountsWithBoard: 0,
+        accountsWithMultiBoard: 0,
+        accountsWithWatchlists: 0,
+        accountsWithWatchlistOnBoard: 0,
         emptyWlCount: 0,
         isTradingLocked: 0,
         closeConfirmation: 0,
-        colorizedQuotes: 0,
         quickTradeEnabled: 0,
         boardsCount: 0,
         wlCount: 0,
@@ -265,10 +296,10 @@ function analyzeFile(data) {
         widgetsConfig: [],
         singleBoardWidgetsConfig: [],
         multiBoardWidgetsConfig: [],
-        wlCountFromBoardByUser: [],
-        instrumentsCountByUser: [],
+        wlCountFromBoardByAccount: [],
+        instrumentsCountByAccount: [],
         widgetsCountsByBoards: [],
-        multiBoardWidgetsConfigByUser: {},
+        multiBoardWidgetsConfigByAccount: {},
       }
     );
 
@@ -277,21 +308,22 @@ function analyzeFile(data) {
       wlCountFromBoard,
       emptyWlCount,
       boardsCount,
-      usersWithBoard,
+      accountsWithBoard,
       wlCount,
-      usersWithWatchlistOnBoard,
+      accountsWithWatchlistOnBoard,
       widgetsConfig,
       singleBoardWidgetsConfig,
       multiBoardWidgetsConfig,
-      wlCountFromBoardByUser,
-      instrumentsCountByUser,
+      wlCountFromBoardByAccount,
+      instrumentsCountByAccount,
       widgetsCountsByBoards,
-      multiBoardWidgetsConfigByUser,
+      multiBoardWidgetsConfigByAccount,
       ...rest
     } = result;
 
     let maxChartsPerBoard = 0;
     let maxMarketDepthPerBoard = 0;
+    console.log({ result });
 
     const chartAndMarketDepthCount = widgetsCountsByBoards.reduce(
       (acc, boards) => {
@@ -324,31 +356,33 @@ function analyzeFile(data) {
       instrumentsCountPerWatchlist: round(
         instrumentsCount / (wlCountFromBoard - emptyWlCount)
       ),
-      boardsPerUser: round(boardsCount / usersWithBoard),
+      boardsPerAccount: round(boardsCount / accountsWithBoard),
       wlPerBoard: round(wlCount / boardsCount),
-      wlPerUser: round(wlCount / usersWithWatchlistOnBoard),
-      wlMedianCountFromBoardByUser: getMedian(wlCountFromBoardByUser),
-      wlCountFromBoardByUser: round(
-        wlCountFromBoardByUser.reduce((acc, c) => acc + c, 0) /
-          wlCountFromBoardByUser.length
+      wlPerAccount: round(wlCount / accountsWithWatchlistOnBoard),
+      wlMedianCountFromBoardByAccount: getMedian(wlCountFromBoardByAccount),
+      wlCountFromBoardByAccount: round(
+        wlCountFromBoardByAccount.reduce((acc, c) => acc + c, 0) /
+          wlCountFromBoardByAccount.length
       ),
-      instrumentsMedianCountByUser: getMedian(instrumentsCountByUser),
-      instrumentsCountByUser: round(
-        instrumentsCountByUser.reduce((acc, c) => acc + c, 0) /
-          instrumentsCountByUser.length
+      instrumentsMedianCountByAccount: getMedian(instrumentsCountByAccount),
+      instrumentsCountByAccount: round(
+        instrumentsCountByAccount.reduce((acc, c) => acc + c, 0) /
+          instrumentsCountByAccount.length
       ),
       chartsCountByBoards: round(
-        chartAndMarketDepthCount.chartCount / widgetsCountsByBoards.length
+        chartAndMarketDepthCount.chartCount /
+          _.flatten(widgetsCountsByBoards).length
       ),
       marketDepthCountByBoards: round(
-        chartAndMarketDepthCount.marketDepthCount / widgetsCountsByBoards.length
+        chartAndMarketDepthCount.marketDepthCount /
+          _.flatten(widgetsCountsByBoards).length
       ),
       maxChartsPerBoard,
       maxMarketDepthPerBoard,
       widgetsConfig: normalizeWlConfig(widgetsConfig),
       singleBoardWidgetsConfig: normalizeWlConfig(singleBoardWidgetsConfig),
       multiBoardWidgetsConfig: normalizeWlConfig(multiBoardWidgetsConfig),
-      multiBoardWidgetsConfigByUser,
+      multiBoardWidgetsConfigByAccount,
     };
 
     renderResult(normalizedResult);
